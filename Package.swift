f2c60178ac2ca98eed460505100b010bfda81632
Matt Gannon <gannonm3211@gmail.com>
2023-09-25 15:13:03 -0600

    Depend on remote MGKeychain repo

--- Package.swift
+++ Package.swift
@@ -13,20 +13,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MGOAuth1",
-            targets: ["MGOAuth1"]),
+            targets: ["MGOAuth1"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../MGKeychain")
+        .package(url: "https://github.com/gannonmg/MGKeychain", from: .init(1, 0, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MGOAuth1",
-            dependencies: ["MGKeychain"]),
-        .testTarget(
-            name: "MGOAuth1Tests",
-            dependencies: ["MGOAuth1"]),
+            dependencies: ["MGKeychain"]
+        )
     ]
 )
